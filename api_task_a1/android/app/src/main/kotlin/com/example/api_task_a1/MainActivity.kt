56bba3436a8abb4da889a5618b4da0969db4b1c1
sharjeel <awansharjeel1122@gmail.com>
2024-02-29 16:32:44 +0430

    first commit

---
+++ api_task_a1/android/app/src/main/kotlin/com/example/api_task_a1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.api_task_a1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ api_task_a1/lib/api_utils/api_utils.dart
@@ -0,0 +1,6 @@
+class ApiUtils {
+  static String prayerUrl =
+      "http://api.aladhan.com/v1/calendarByCity/2017/4?city=Abbottabad&country=Pakistan&method=2";
+
+// static String productUrl = "${url}products";
+}
